c0d1a5629c5f1c0e55fc7dc5011aa6a59e8ea522
DeLeon012 <114931493+DeLeon012@users.noreply.github.com>
2023-04-25 08:45:31 +0300

    Update test_pyglet.py

--- space/test_pyglet.py
+++ space/test_pyglet.py
@@ -1,14 +1,13 @@
-import time
-
 import pyglet
 from my_math import *
 from math import *
-import keyboard
 
-window = pyglet.window.Window(width=1000, height=1000, fullscreen=False)
+
+window = pyglet.window.Window(
+    fullscreen=True)
 circle = pyglet.shapes.Circle
 
-G = 10 ** (-2)
+G = 5
 p_V = 0.3
 T = 0.5
 
@@ -16,14 +15,14 @@ STOP = False
 
 
 class ball:
-    def __init__(self, poss, v=vector(5, 5), mass=5_000, color=(200, 200, 200), p_v1=p_V):
+    def __init__(self, poss, v=vector(5, 5), mass=5_000, color=None, p_v1=p_V):
         self.poss = poss
         self.v = v
         self.mass = mass
         self.p_v = p_v1
         self.R = (3 * mass / (4 * self.p_v * pi)) ** (1 / 3)
         self.a = vector(0, 0)
-        self.color = color
+        self.color = get_color(mass) if not color else color
 
     def F(self, ball2):
         r = ball2.poss - self.poss
@@ -43,39 +42,36 @@ class ball:
         if self.poss.y <= self.R or self.poss.y >= window.height - self.R:
             self.v.y *= -1
 
+        self.a = vector(0, 0)
+
+
+def get_color(mass):
+    k = (-778) / (mass + 778) + 1
+    r = 240 + int((93 - 240) * k)
+    g = 116 + int((127 - 116) * k)
+    b = 82 + int((247 - 82) * k)
+    return r, g, b
+
 
 items = [
-    ball(vector(window.width // 2 + 50, window.height // 2), v=vector(0, 5), color=(200, 50, 50)),
-    ball(vector(window.width // 2 - 50, window.height // 2), v=vector(0, -5), color=(50, 200, 50)),
-    ball(vector(window.width // 2 - 50, window.height // 2 - 50), v=vector(-5, 0), color=(50, 50, 200))
+    ball(vector(window.width // 2 + 50, window.height // 2), v=vector(0, 5), mass=7000),
+    ball(vector(window.width // 2 - 50, window.height // 2), v=vector(0, -5), mass=5000),
+    ball(vector(window.width // 2 - 50, window.height // 2 - 50), v=vector(-5, 0), mass=1000)
 ]
 
 
 @window.event
 def on_draw():
     window.clear()
-
-    for i in items:
-        circle(*i.poss, i.R, color=i.color).draw()
-
-    while STOP:
-        time.sleep(0.5)
-
     for i in range(len(items)):
         for j in range(len(items)):
             if i == j:
                 continue
             items[i].F(items[j])
         items[i].update()
-
-
-def f(arg):
-    global STOP
-    print(f'Нажали {arg}')
-    STOP = arg
+    for i in items:
+        circle(*i.poss, i.R, color=i.color).draw()
 
 
 if __name__ == '__main__':
-    keyboard.add_hotkey('ctrl', f, args=True)
-    keyboard.add_hotkey('alt', f, args=False)
     pyglet.app.run()
